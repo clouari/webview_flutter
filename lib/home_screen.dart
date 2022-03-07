ee6b03232deeaec5fff3a792e2eec67d1d5da833
Da hee <ekdudekgml35@naver.com>
2022-03-07 10:35:23 +0900

    webview: homeScreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:webview/find_zip_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('우편번호 검색'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              const SizedBox(
+                width: 100,
+                child: TextField(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FindZipScreen()),
+                  );
+                },
+                child: const Text('우편번호 찾기'),
+              ),
+            ],
+          ),
+          const TextField(),
+          Row(
+            children: const [
+              Expanded(child: TextField()),
+              SizedBox(width: 30),
+              Expanded(child: TextField()),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
